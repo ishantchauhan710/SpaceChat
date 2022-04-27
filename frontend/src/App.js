2f738f2561842d85ea35c9ae71fedf2a4c749ce9
ishant <ishantchauhan710@gmail.com>
2022-04-27 21:32:10 +0530

    LoginPage Side Screen Designed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { LoginPage } from './pages/LoginPage';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <LoginPage />
     </div>
   );
 }
