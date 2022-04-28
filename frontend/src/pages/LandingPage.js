83ce18c37af497f6bd2a594753475f1ad97c84fb
ishant <ishantchauhan710@gmail.com>
2022-04-28 20:58:27 +0530

    Lottie Animation on Landing Page Implemented

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,7 +1,11 @@
 import { Button } from '@mui/material';
 import React from 'react';
+import Lottie from "lottie-react";
+import landingPageAnimation from "../data/anim/landing_page_animation.json";
 
 export const LandingPage = () => {
+
+
   return (
     <div className='container-landing-page'>
       
@@ -10,14 +14,16 @@ export const LandingPage = () => {
        <div className='login-content'>
           <span className='text-h1'>Discover friends and people around you</span>
           <span className='text-h2'>SpaceChat is the place where you can meet and chat with anyone in this world! Built using the power of NodeJs and Socket.io, this app is completely free and open source!</span>
-          <Button variant="contained" style={{marginTop: 20, fontSize: 18, fontWeight: "bold", textTransform: "none",backgroundColor: "#388e3c"}}>Get Started</Button>
-          <Button variant="outlined" style={{marginTop: 20, marginLeft: 20, fontSize: 18, fontWeight: "bold", textTransform: "none",borderColor: "#ffffff", color: "#ffffff"}}>View Source Code</Button>
+          <div className='container-landing-page-buttons'>
+             <Button variant="contained" style={{fontSize: 18, fontWeight: "bold", textTransform: "none",backgroundColor: "#388e3c"}}>Get Started</Button>
+             <Button variant="outlined" style={{marginLeft: 20, fontSize: 18, fontWeight: "bold", textTransform: "none",borderColor: "#ffffff", color: "#ffffff"}}>View Source Code</Button>
+          </div>
        </div>
 
       </div>
 
       <div className='container-login-animation'>
-          Login
+          <Lottie autoPlay={true} loop={true} animationData={landingPageAnimation} />
       </div>
 
       
