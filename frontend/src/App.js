c6e1bae8ddaa6d4e8a595800ac1e5a8eb55ae24b
ishant <ishantchauhan710@gmail.com>
2022-04-29 23:35:28 +0530

    React Router DOM Setup Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,23 @@ import { ThemeProvider } from "@emotion/react";
 import { createTheme } from "@mui/system";
 import "./App.css";
 import { LandingPage } from "./pages/LandingPage";
+import { HomePage } from "./pages/HomePage";
+
 import { appTheme } from "./data/themes/appTheme";
 import "./data/color/appColor.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   const mainTheme = createTheme(appTheme);
 
   return (
     <ThemeProvider theme={mainTheme}>
-      <div className="App">
-        <LandingPage />
-      </div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
