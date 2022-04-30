c4c1cc982fcbc0232ba76af919c1d15d3b256c91
ishant <ishantchauhan710@gmail.com>
2022-04-30 18:23:49 +0530

    Login User API Created

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -20,7 +20,7 @@ const signupUserController = expressAsyncHandler(async (req, res) => {
     throw new Error("Password cannot be blank");
   }
 
-  const userExists = await User.findOne({ sc_userEmail:userEmail });
+  const userExists = await User.findOne({ sc_userEmail: userEmail });
 
   if (userExists) {
     res.status(400);
@@ -28,10 +28,10 @@ const signupUserController = expressAsyncHandler(async (req, res) => {
   }
 
   const user = await User.create({
-    sc_userName:userName,
-    sc_userEmail:userEmail,
-    sc_userPassword:userPassword,
-    sc_userProfilePicture:userProfilePicture,
+    sc_userName: userName,
+    sc_userEmail: userEmail,
+    sc_userPassword: userPassword,
+    sc_userProfilePicture: userProfilePicture,
   });
 
   if (user) {
@@ -40,14 +40,43 @@ const signupUserController = expressAsyncHandler(async (req, res) => {
       sc_userName: user.sc_userEmail,
       sc_userEmail: user.sc_userEmail,
       sc_userProfilePicture: user.sc_userProfilePicture,
-      token: generateToken(user._id)
-    })
+      token: generateToken(user._id),
+    });
   } else {
-      res.status(400);
-      throw new Error("Unable to create account");
+    res.status(400);
+    throw new Error("Unable to create account");
   }
 });
 
-const loginUserController = expressAsyncHandler(async (req, res) => {});
+const loginUserController = expressAsyncHandler(async (req, res) => {
+  const { userEmail, userPassword } = req.body;
+
+  if (!userEmail) {
+    res.status(400);
+    throw new Error("Email cannot be blank");
+  }
+
+  if (!userPassword) {
+    res.status(400);
+    throw new Error("Password cannot be blank");
+  }
+
+  const dbUser = await User.findOne({ sc_userEmail: userEmail });
+
+  if (dbUser) {
+    if (await dbUser.matchPassword(userPassword)) {
+      res.status(201).json({
+        _id: user._id,
+        userName: user.sc_userName,
+        userEmail: user.sc_userEmail,
+        userProfilePicture: user.sc_userProfilePicture,
+        token: generateToken(user._id),
+      });
+    }
+  } else {
+    res.status(400);
+    throw new Error("No user with the provided email exists");
+  }
+});
 
 module.exports = { signupUserController, loginUserController };
